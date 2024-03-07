7e8ca6dfe0ce0dba208c43944bd9963ad4c484e1
ivan <ivansemenuha@gmail.com>
2024-03-07 13:11:32 +0300

    table

---
+++ client/src/_mocks/employees.ts
@@ -0,0 +1,18 @@
+const employees = [
+  {
+    id: 1,
+    name: 'Benyamin Gilbeart',
+    position: 'Web Designer III',
+    email: 'bgilbeart0@bravesites.com',
+    salary: 164740,
+  },
+  {
+    id: 2,
+    name: 'Averil Pepineaux',
+    position: 'Web Designer III',
+    email: 'apepineaux1@liveinternet.ru',
+    salary: 240001,
+  },
+];
+
+export default employees;
--- client/src/components/testDrive/index.tsx
+++ client/src/components/testDrive/index.tsx
@@ -6,23 +6,23 @@ import Button from '@mui/material/Button';
 import CardHeader from '../cardHeader';
 
 export default function testDrive() {
-	return (
-		<Card>
-			<CardHeader
-				title="Записаться на тест-драйв"
-				size="small"
-				sx={{
-					mb: 2,
-				}}
-			/>
-			<Stack spacing={2} direction="column">
-				<TextField id="outlined-basic" placeholder="Ваше имя" variant="outlined" />
-				<TextField id="outlined-basic" placeholder="Ваш Email" variant="outlined" />
-				<TextField placeholder="Ваши пожелания" multiline minRows={4} />
-				<Button variant="contained" disableElevation>
-					Записаться
-				</Button>
-			</Stack>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader
+        title="Записаться на тест-драйв"
+        size="small"
+        sx={{
+          mb: 2,
+        }}
+      />
+      <Stack spacing={2} direction="column">
+        <TextField id="outlined-basic" placeholder="Ваше имя" variant="outlined" />
+        <TextField id="phone-input" placeholder="Ваш телефон" variant="outlined" type="tel" />
+        <TextField placeholder="Ваши пожелания" multiline minRows={4} />
+        <Button variant="contained" disableElevation>
+          Записаться
+        </Button>
+      </Stack>
+    </Card>
+  );
 }
