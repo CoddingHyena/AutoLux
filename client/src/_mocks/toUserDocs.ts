be850ccefda273dac8a95e91b05230cacc8aac9b
ivan <ivansemenuha@gmail.com>
2024-03-07 18:44:54 +0300

    profile1

---
+++ client/src/_mocks/toUserDocs.ts
@@ -0,0 +1,25 @@
+const toUserDocs = [
+  {
+    id: 1,
+    dateNow: '2024-02-06',
+    car_id: 1,
+    userScore: 4,
+    userComment: 'Комментарий 1',
+  },
+  {
+    id: 3,
+    dateNow: '2024-02-06',
+    car_id: 1,
+    userScore: 2,
+    userComment: 'Комментарий 2',
+  },
+  {
+    id: 4,
+    dateNow: '2024-02-06',
+    car_id: 1,
+    userScore: 3,
+    userComment: 'Комментарий 2',
+  },
+];
+
+export default toUserDocs;
--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -8,19 +8,122 @@ import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Switch from '@mui/material/Switch';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import DataTable from '../../components/dataTable';
+
+import AddIcon from '@mui/icons-material/Add';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import PersonOffOutlinedIcon from '@mui/icons-material/PersonOffOutlined';
+
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 import EditIcon from '@mui/icons-material/Edit';
 import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
 import CardHeader from '../../components/cardHeader';
+import toUserDocs from '../../_mocks/toUserDocs';
+import userCars from '../../_mocks/cars';
 
-function Account() {
+const getHeadCellsTO = [
+  {
+    id: 'id',
+    numeric: false,
+    disablePadding: false,
+    label: 'Id',
+  },
+  {
+    id: 'date',
+    numeric: false,
+    disablePadding: false,
+    label: 'Дата',
+  },
+  {
+    id: 'carId',
+    numeric: false,
+    disablePadding: false,
+    label: 'Автомобиль',
+  },
+  {
+    id: 'userScore',
+    numeric: false,
+    disablePadding: false,
+    label: 'Оценка',
+  },
+  {
+    id: 'userComment',
+    numeric: false,
+    disablePadding: false,
+    label: 'Комментарий',
+  },
+];
+
+const getHeadCellsUserAuto = [
+  {
+    id: 'id',
+    numeric: false,
+    disablePadding: false,
+    label: 'Id',
+  },
+  {
+    id: 'mark',
+    numeric: false,
+    disablePadding: false,
+    label: 'Марка',
+  },
+  {
+    id: 'model',
+    numeric: false,
+    disablePadding: false,
+    label: 'Модель',
+  },
+  {
+    id: 'color',
+    numeric: false,
+    disablePadding: false,
+    label: 'Цвет',
+  },
+  {
+    id: 'prodYear',
+    numeric: false,
+    disablePadding: false,
+    label: 'Дата выпуска',
+  },
+  {
+    id: 'gosNum',
+    numeric: false,
+    disablePadding: false,
+    label: 'Госномер',
+  },
+  {
+    id: 'gear',
+    numeric: false,
+    disablePadding: false,
+    label: 'Коробка',
+  },
+  {
+    id: 'engine',
+    numeric: false,
+    disablePadding: false,
+    label: 'Двигатель',
+  },
+  {
+    id: 'vin',
+    numeric: false,
+    disablePadding: false,
+    label: 'VIN',
+  },
+];
+
+export default function Account() {
   return (
     <Stack spacing={6} sx={{ marginTop: 4 }}>
       <GeneralSettingsSection />
-      <ProfileDocsSection />
-      <ProfileRequestsSection />
+      <UserDocsToTable />
+      <UserDocsTestDriveTable />
+      <UserAutoTable />
     </Stack>
   );
 }
@@ -64,30 +167,142 @@ function GeneralSettingsSection() {
   );
 }
 
-function ProfileDocsSection() {
+// GET('/api/lk/DocTestDrive')
+// GET('/api/lk/DocTO')
+//  возвращают массив с элементами
+//  atributes: ['id', 'dateNow', 'user_id', 'car_id', 'userScore', 'userComment']
+
+function UserDocsToTable({ name, props }) {
   return (
-    <Card type="section">
-      <CardHeader title="Ваши документы" />
-      <Stack spacing={3}>
-        <h2>Тип доков 1</h2>
-        <p>Здесь позже разместим доки типа 1 в табличном виде</p>
-        <Divider />
-        <h2>Тип доков 2</h2>
-        <p>Здесь позже разместим доки типа 2 в табличном виде</p>
-      </Stack>
+    <Card component="section" type="section">
+      <CardHeader title="Документы на Техобслуживание" subtitle=""></CardHeader>
+      <DataTable
+        {...props}
+        headCells={getHeadCellsTO}
+        rows={toUserDocs}
+        emptyRowsHeight={{ default: 66.8, dense: 46.8 }}
+        render={(row) => (
+          <TableRow hover tabIndex={-1} key={row.id}>
+            <TableCell>{row.id}</TableCell>
+            <TableCell align="left">{row.dateNow}</TableCell>
+            <TableCell align="left">{row?.car_id}</TableCell>
+            <TableCell align="left">{row?.userScore}</TableCell>
+            <TableCell align="left">{row?.userComment}</TableCell>
+            {/* <TableCell align="right">${row.salary.toLocaleString()}</TableCell> */}
+            <TableCell align="right">
+              <Tooltip title="Редактировать" arrow>
+                <IconButton
+                  aria-label="edit"
+                  color="warning"
+                  size="small"
+                  sx={{ fontSize: 2 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <ModeEditOutlineOutlinedIcon fontSize="medium" />
+                </IconButton>
+              </Tooltip>
+            </TableCell>
+          </TableRow>
+        )}
+      />
     </Card>
   );
 }
 
-function ProfileRequestsSection() {
+function UserDocsTestDriveTable({ name, props }) {
   return (
-    <Card type="section">
-      <CardHeader title="Ваши обращения" />
-      <Stack spacing={3}>
-        <p>Здесь позже разместим обращения в табличном виде</p>
-      </Stack>
+    <Card component="section" type="section">
+      <CardHeader title="Тестдрайв" subtitle=""></CardHeader>
+      <DataTable
+        {...props}
+        headCells={getHeadCellsTO}
+        rows={toUserDocs}
+        emptyRowsHeight={{ default: 66.8, dense: 46.8 }}
+        render={(row) => (
+          <TableRow hover tabIndex={-1} key={row.id}>
+            <TableCell>{row.id}</TableCell>
+            <TableCell align="left">{row.dateNow}</TableCell>
+            <TableCell align="left">{row?.car_id}</TableCell>
+            <TableCell align="left">{row?.userScore}</TableCell>
+            <TableCell align="left">{row?.userComment}</TableCell>
+            {/* <TableCell align="right">${row.salary.toLocaleString()}</TableCell> */}
+            <TableCell align="right">
+              <Tooltip title="Редактировать" arrow>
+                <IconButton
+                  aria-label="edit"
+                  color="warning"
+                  size="small"
+                  sx={{ fontSize: 2 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <ModeEditOutlineOutlinedIcon fontSize="medium" />
+                </IconButton>
+              </Tooltip>
+            </TableCell>
+          </TableRow>
+        )}
+      />
     </Card>
   );
 }
 
-export default Account;
+function UserAutoTable({ name, props }) {
+  return (
+    <Card component="section" type="section">
+      <CardHeader title="Мои автомобили" subtitle=""></CardHeader>
+      <DataTable
+        {...props}
+        headCells={getHeadCellsUserAuto}
+        rows={userCars}
+        emptyRowsHeight={{ default: 66.8, dense: 46.8 }}
+        render={(row) => (
+          <TableRow hover tabIndex={-1} key={row.id}>
+            <TableCell>{row.id}</TableCell>
+            <TableCell align="left">{row.mark}</TableCell>
+            <TableCell align="left">{row?.model}</TableCell>
+            <TableCell align="left">{row?.color}</TableCell>
+            <TableCell align="left">{row?.prodYear}</TableCell>
+            <TableCell align="left">{row.gosNum}</TableCell>
+            <TableCell align="left">{row?.gear}</TableCell>
+            <TableCell align="left">{row?.engine}</TableCell>
+            <TableCell align="left">{row?.vin}</TableCell>
+            {/* <TableCell align="right">${row.salary.toLocaleString()}</TableCell> */}
+            <TableCell align="right">
+              <Tooltip title="Редактировать" arrow>
+                <IconButton
+                  aria-label="edit"
+                  color="warning"
+                  size="small"
+                  sx={{ fontSize: 2 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <ModeEditOutlineOutlinedIcon fontSize="medium" />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Удалить" arrow>
+                <IconButton
+                  aria-label="edit"
+                  color="error"
+                  size="small"
+                  sx={{ fontSize: 2 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <PersonOffOutlinedIcon fontSize="medium" />
+                </IconButton>
+              </Tooltip>
+            </TableCell>
+          </TableRow>
+        )}
+      />
+    </Card>
+  );
+}
